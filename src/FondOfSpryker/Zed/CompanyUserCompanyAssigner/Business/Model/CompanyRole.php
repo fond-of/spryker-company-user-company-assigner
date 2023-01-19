bea6ec22e7da5e07c7ba474e7149be7ee536acd7
Jozsef Geng <jozsef.geng@fondof.de>
2023-01-19 12:08:16 +0100

    add Company Role basic implementation

---
+++ src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Business/Model/CompanyRole.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace FondOfSpryker\Zed\CompanyUserCompanyAssigner\Business\Model;
+
+
+use FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyFacadeInterface;
+use FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyRoleFacadeInterface;
+use FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyTypeFacadeInterface;
+use FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyUserFacadeInterface;
+use Generated\Shared\Transfer\CompanyRoleCollectionTransfer;
+use Generated\Shared\Transfer\CompanyRoleCriteriaFilterTransfer;
+use Generated\Shared\Transfer\CompanyUserTransfer;
+
+class CompanyRole implements CompanyRoleInterface
+{
+    /**
+     * @var \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyFacadeInterface
+     */
+    protected $companyFacade;
+
+    /**
+     * @var \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyRoleFacadeInterface
+     */
+    protected $companyRoleFacade;
+
+    /**
+     * @var \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyTypeFacadeInterface
+     */
+    protected $companyTypeFacade;
+
+    /**
+     * @var \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Business\Model\CompanyUser
+     */
+    protected $companyUser;
+
+    /**
+     * @var \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyUserFacadeInterface
+     */
+    protected $companyUserFacade;
+
+    /**
+     * @param \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyFacadeInterface $companyFacade
+     * @param \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyRoleFacadeInterface $companyRoleFacade
+     * @param \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyTypeFacadeInterface $companyTypeFacade
+     * @param \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Business\Model\CompanyUser $companyUser
+     * @param \FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade\CompanyUserCompanyAssignerToCompanyUserFacadeInterface $companyUserFacade
+     */
+    public function __construct(
+        CompanyUserCompanyAssignerToCompanyFacadeInterface $companyFacade,
+        CompanyUserCompanyAssignerToCompanyRoleFacadeInterface $companyRoleFacade,
+        CompanyUserCompanyAssignerToCompanyTypeFacadeInterface $companyTypeFacade,
+        CompanyUser $companyUser,
+        CompanyUserCompanyAssignerToCompanyUserFacadeInterface $companyUserFacade
+    ) {
+        $this->companyFacade = $companyFacade;
+        $this->companyRoleFacade = $companyRoleFacade;
+        $this->companyTypeFacade = $companyTypeFacade;
+        $this->companyUser = $companyUser;
+        $this->companyUserFacade = $companyUserFacade;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUserTransfer $companyUserTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyRoleCollectionTransfer
+     */
+    public function getCompanyUserRoleCollection(
+        CompanyUserTransfer $companyUserTransfer
+    ): CompanyRoleCollectionTransfer {
+         return $this->companyRoleFacade
+             ->getCompanyRoleCollection(
+                 (new CompanyRoleCriteriaFilterTransfer())
+                     ->setIdCompanyUser($companyUserTransfer->getIdCompanyUser()),
+             );
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUserTransfer $companyUserTransfer
+     *
+     * @return void
+     */
+    public function updateNonManufacturerUsersCompanyRole(CompanyUserTransfer $manufacturerCompanyUserTransfer): void
+    {}
+
+}
