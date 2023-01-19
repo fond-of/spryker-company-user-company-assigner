983ce46044089ed35dd746f55831f498dfea28a3
Jozsef Geng <jozsef.geng@fondof.de>
2023-01-19 12:20:03 +0100

    Ad repository

--- src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Persistence/CompanyUserCompanyAssignerRepository.php
+++ src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Persistence/CompanyUserCompanyAssignerRepository.php
@@ -4,13 +4,18 @@ namespace FondOfSpryker\Zed\CompanyUserCompanyAssigner\Persistence;
 
 use Generated\Shared\Transfer\CompanyRoleTransfer;
 use Generated\Shared\Transfer\CompanyTransfer;
+use Generated\Shared\Transfer\CompanyUserCollectionTransfer;
+use Generated\Shared\Transfer\CompanyUserCriteriaFilterTransfer;
 use Generated\Shared\Transfer\CompanyUserTransfer;
 use Generated\Shared\Transfer\CustomerTransfer;
+use Generated\Shared\Transfer\PaginationTransfer;
 use Orm\Zed\Company\Persistence\Map\SpyCompanyTableMap;
 use Orm\Zed\CompanyBusinessUnit\Persistence\Map\SpyCompanyBusinessUnitTableMap;
 use Orm\Zed\CompanyRole\Persistence\Map\SpyCompanyRoleTableMap;
 use Orm\Zed\CompanyType\Persistence\Map\FosCompanyTypeTableMap;
+use Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\ActiveQuery\ModelCriteria;
 use Spryker\Zed\Kernel\Persistence\AbstractRepository;
 
 /**
@@ -157,5 +162,98 @@ class CompanyUserCompanyAssignerRepository extends AbstractRepository implements
             ->filterByIdCompanyRole($idCompanyRole)
             ->select([SpyCompanyRoleTableMap::COL_NAME])
             ->findOne();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CompanyUserCriteriaFilterTransfer $criteriaFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyUserCollectionTransfer
+     */
+    public function getCompanyUserCollectionByCompanyUserCriteriaFilterTransfer(
+        CompanyUserCriteriaFilterTransfer $companyUserCriteriaTransfer
+    ): CompanyUserCollectionTransfer {
+        /** @var \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery $queryCompanyUser */
+        $queryCompanyUser = $this->getFactory()
+            ->getCompanyUserQuery()
+            ->joinWithCustomer()
+            ->useCustomerQuery()
+            ->filterByAnonymizedAt(null, Criteria::ISNULL)
+            ->endUse();
+
+        $this->applyFilters($queryCompanyUser, $companyUserCriteriaTransfer);
+
+        $collection = $this->buildQueryFromCriteria($queryCompanyUser, $companyUserCriteriaTransfer->getFilter());
+        /** @var array<\Generated\Shared\Transfer\SpyCompanyUserEntityTransfer> $companyUserCollection */
+        $companyUserCollection = $this->getPaginatedCollection($collection, $companyUserCriteriaTransfer->getPagination());
+
+        $collectionTransfer = $this->getFactory()
+            ->createCompanyUserMapper()
+            ->mapCompanyUserCollection($companyUserCollection);
+
+        $collectionTransfer->setPagination($companyUserCriteriaTransfer->getPagination());
+
+        return $collectionTransfer;
+    }
+
+    /**
+     * @param \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery $queryCompanyUser
+     * @param \Generated\Shared\Transfer\CompanyUserCriteriaFilterTransfer $criteriaFilterTransfer
+     *
+     * @return void
+     */
+    protected function applyFilters(SpyCompanyUserQuery $queryCompanyUser, CompanyUserCriteriaFilterTransfer $criteriaFilterTransfer): void
+    {
+        if ($criteriaFilterTransfer->getIdCompany() !== null) {
+            $queryCompanyUser->filterByFkCompany($criteriaFilterTransfer->getIdCompany());
+        }
+
+        if ($criteriaFilterTransfer->getIdCustomer() !== null) {
+            $queryCompanyUser->filterByFkCustomer($criteriaFilterTransfer->getIdCustomer());
+        }
+
+        if ($criteriaFilterTransfer->getCompanyUserIds()) {
+            $queryCompanyUser->filterByIdCompanyUser_In($criteriaFilterTransfer->getCompanyUserIds());
+        }
+
+        if ($criteriaFilterTransfer->getIsActive() !== null) {
+            $queryCompanyUser->filterByIsActive($criteriaFilterTransfer->getIsActive());
+        }
+    }
+
+    /**
+     * @param \Propel\Runtime\ActiveQuery\ModelCriteria $query
+     * @param \Generated\Shared\Transfer\PaginationTransfer|null $paginationTransfer
+     *
+     * @return \Propel\Runtime\Collection\Collection|\Propel\Runtime\Collection\ObjectCollection|\Propel\Runtime\ActiveRecord\ActiveRecordInterface[]
+     */
+    protected function getPaginatedCollection(ModelCriteria $query, ?PaginationTransfer $paginationTransfer = null)
+    {
+        if ($paginationTransfer !== null) {
+            $page = $paginationTransfer
+                ->requirePage()
+                ->getPage();
+
+            $maxPerPage = $paginationTransfer
+                ->requireMaxPerPage()
+                ->getMaxPerPage();
+
+            $paginationModel = $query->paginate($page, $maxPerPage);
+
+            $paginationTransfer->setNbResults($paginationModel->getNbResults());
+            $paginationTransfer->setFirstIndex($paginationModel->getFirstIndex());
+            $paginationTransfer->setLastIndex($paginationModel->getLastIndex());
+            $paginationTransfer->setFirstPage($paginationModel->getFirstPage());
+            $paginationTransfer->setLastPage($paginationModel->getLastPage());
+            $paginationTransfer->setNextPage($paginationModel->getNextPage());
+            $paginationTransfer->setPreviousPage($paginationModel->getPreviousPage());
+
+            return $paginationModel->getResults();
+        }
+
+        return $query->find();
     }
 }
