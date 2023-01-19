ad467a83c2509ce2fbe931a1a435ff42f56aed45
Jozsef Geng <jozsef.geng@fondof.de>
2023-01-19 12:17:31 +0100

    commit facade Interface

--- src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Dependency/Facade/CompanyUserCompanyAssignerToCompanyRoleFacadeInterface.php
+++ src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Dependency/Facade/CompanyUserCompanyAssignerToCompanyRoleFacadeInterface.php
@@ -2,7 +2,10 @@
 
 namespace FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade;
 
+use Generated\Shared\Transfer\CompanyRoleCollectionTransfer;
+use Generated\Shared\Transfer\CompanyRoleCriteriaFilterTransfer;
 use Generated\Shared\Transfer\CompanyRoleTransfer;
+use Generated\Shared\Transfer\CompanyUserTransfer;
 
 interface CompanyUserCompanyAssignerToCompanyRoleFacadeInterface
 {
@@ -12,4 +15,20 @@ interface CompanyUserCompanyAssignerToCompanyRoleFacadeInterface
      * @return \Generated\Shared\Transfer\CompanyRoleTransfer
      */
     public function getCompanyRoleById(CompanyRoleTransfer $companyRoleTransfer): CompanyRoleTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyRoleCriteriaFilterTransfer $criteriaFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyRoleCollectionTransfer
+     */
+    public function getCompanyRoleCollection(
+        CompanyRoleCriteriaFilterTransfer $criteriaFilterTransfer
+    ): CompanyRoleCollectionTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUserTransfer $companyUserTransfer
+     *
+     * @return void
+     */
+    public function saveCompanyUser(CompanyUserTransfer $companyUserTransfer): void;
 }
