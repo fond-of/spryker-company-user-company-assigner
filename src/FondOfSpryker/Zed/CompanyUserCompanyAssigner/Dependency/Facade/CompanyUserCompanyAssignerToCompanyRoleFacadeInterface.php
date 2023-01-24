b112b51506541dd3e9c3e4db99855cc20df719b7
Jozsef Geng <jozsef.geng@fondof.de>
2023-01-24 18:16:25 +0100

    CompanyUserCompanyAssignerToCompanyRoleFacadeBridge

--- src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Dependency/Facade/CompanyUserCompanyAssignerToCompanyRoleFacadeInterface.php
+++ src/FondOfSpryker/Zed/CompanyUserCompanyAssigner/Dependency/Facade/CompanyUserCompanyAssignerToCompanyRoleFacadeInterface.php
@@ -2,8 +2,6 @@
 
 namespace FondOfSpryker\Zed\CompanyUserCompanyAssigner\Dependency\Facade;
 
-use Generated\Shared\Transfer\CompanyRoleCollectionTransfer;
-use Generated\Shared\Transfer\CompanyRoleCriteriaFilterTransfer;
 use Generated\Shared\Transfer\CompanyRoleTransfer;
 use Generated\Shared\Transfer\CompanyUserTransfer;
 
@@ -15,15 +13,6 @@ interface CompanyUserCompanyAssignerToCompanyRoleFacadeInterface
      * @return \Generated\Shared\Transfer\CompanyRoleTransfer
      */
     public function getCompanyRoleById(CompanyRoleTransfer $companyRoleTransfer): CompanyRoleTransfer;
-
-    /**
-     * @param \Generated\Shared\Transfer\CompanyRoleCriteriaFilterTransfer $criteriaFilterTransfer
-     *
-     * @return \Generated\Shared\Transfer\CompanyRoleCollectionTransfer
-     */
-    public function getCompanyRoleCollection(
-        CompanyRoleCriteriaFilterTransfer $criteriaFilterTransfer
-    ): CompanyRoleCollectionTransfer;
 
     /**
      * @param \Generated\Shared\Transfer\CompanyUserTransfer $companyUserTransfer
